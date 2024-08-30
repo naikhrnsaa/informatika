22beba3a409af1f76235f36d815a3c5b08e56ab9
nakai <145311588+naikhrnsaa@users.noreply.github.com>
2024-08-30 10:16:48 +0700

    Add files via upload

---
+++ Implementasi Materi 1.py
@@ -0,0 +1,19 @@
+#penulisan statement
+print("Hello World")
+
+#penulisan string
+Judul = "Jalan ke Malioboro"
+print(Judul)
+
+#penulisan case
+method_name = "Naila Khairun Nisa Khafitri"
+print(method_name)
+
+#penulisan blok program
+a = 25
+b = 45
+if b > a:
+    print("b is greater than a")
+
+#penulisan komentar
+"""Menggunakan Triple Tanda Petik"""
